db7245a9f67e78a573e1f100cf8148ccbb479ada
Qun Li Huang <qunli02@gmail.com>
2019-04-02 19:17:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def roll_call_dwarves(names)
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(call)
   # Your code here
+  call.map do |word|
+    
 end
 
 def long_planeteer_calls# code an argument here
