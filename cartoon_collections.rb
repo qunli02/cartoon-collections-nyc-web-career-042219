ec144614de5dba67ac87a313e1e4152d40de2120
Qun Li Huang <qunli02@gmail.com>
2019-04-02 19:37:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(names)
   # Your code here
   names.each_with_index do |name, i|
-    puts (names.index(name) + 1).to_s + ". " + name
+    puts i.tos + ". " + name
   end
 end
 
